7a2c77731b4a4f627db677d8cc0ca87c3f6e4e0f
Eugene Oliveros <yujin@jinyuz.dev>
2024-09-08 23:41:07 +0800

    add sample repo

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
+from pathlib import Path
 from litestar import get, Litestar
+from litestar.response import Template
+from litestar.template import TemplateConfig
+
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.contrib.htmx.request import HTMXRequest
+from litestar.contrib.htmx.response import HTMXTemplate
 
 
 @get("/")
-async def index() -> str:
-    return "Hello, world"
+async def index() -> Template:
+    return Template(template_name="pages/index.html")
 
 
-@get("/books/{book_id:int}")
-async def get_book(book_id: int) -> dict[str, int]:
-    return {"book_id": book_id}
+@get(path="/form")
+async def get_form(request: HTMXRequest) -> Template:
+    htmx = request.htmx  #  if true will return HTMXDetails class object
+    if htmx:
+        print(htmx.current_url)
+
+    context = {}
+    return HTMXTemplate(
+        template_name="partials/sample.html",
+        context=context,
+        push_url="/form",
+    )
 
 
-app = Litestar([index, get_book])
+template_config = TemplateConfig(
+    directory=Path(__file__).parent / "templates",
+    engine=JinjaTemplateEngine,
+)
+
+
+app = Litestar(
+    route_handlers=[index, get_form],
+    template_config=template_config,
+)
